28ae2bb0e7f0d9f558f7ad8eccbacb3f0e783010
1day1 <onedayone@gmail.com>
2024-09-10 12:32:47 +0900

    Refactor app module to enable CORS for specific domains

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,22 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
+
+  // 모든 요청에 대해 CORS 허용
+  // app.enableCors();
+  // 특정 도메인만 허용하려면:
+  app.enableCors({
+    origin: [
+      'http://localhost:3001', 
+      'http://localhost:3000', 
+      'https://netflix-next.coolify.1day1.org',
+    ],
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: false, // 인증 정보를 포함하지 않음
+    // credentials: true,  // 쿠키를 포함한 요청 허용 여부
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
